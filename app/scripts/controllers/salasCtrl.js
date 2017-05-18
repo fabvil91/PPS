d2391532fd2773720f7cd9d967bd451faec0571e
Fabrizio Villella <fabrizio.villella.91@gmail.com>
2017-05-18 12:21:37 -0300

    Nuevo estado: seleccionado

--- app/scripts/controllers/salasCtrl.js
+++ app/scripts/controllers/salasCtrl.js
@@ -2,30 +2,33 @@
 	'use strict';
 	angular.module('cine')
 	.controller('salasCtrl', ['$rootScope','$scope','Datos','$sce','Salas','$timeout',function($rootScope,$scope,Datos,$sce,Salas,$timeout){	
-		       
-		    $scope.sala = {};
-        	(function(){
-        		$scope.sala = Salas.crear(10,10);
-        		console.log($scope.sala);
-        	})();
-
-        	var _startCountdown = function(){
+			 $scope.funcion = Datos.listado();
+			 $scope.funcion.sala = Salas.crear(10,10);
+			            	
+	         var _startCountdown = function(){
 				var timerCount = 3660;
 
 				var countDown = function () {
-				if (timerCount < 0) {
-				  //Any desired function upon countdown end.
-				  console.log("SE TERMINO EL TIEMPO");
-				} else {
-				  $scope.countDownLeft = timerCount;
-				  timerCount--;
-				  $timeout(countDown, 1000);
-				}
-				};
-			$scope.countDownLeft = timerCount;
-			countDown();
-			}
-        
-        _startCountdown();
+				 if (timerCount < 0) {					 
+					console.log("SE TERMINO EL TIEMPO");
+				 } else {
+					$scope.countDownLeft = timerCount;
+					timerCount--;
+					$timeout(countDown, 1000);
+				  }
+				 };
+				$scope.countDownLeft = timerCount;
+				countDown();
+			 }
+	        
+	         _startCountdown();
+
+	         $scope.seleccionar = function(asiento){
+	         	if(asiento.booked == false){	         		
+	         		asiento.checked = !asiento.checked;	
+	         		console.log(asiento);
+	         	}
+	         	
+	         }
     }])
 })();
