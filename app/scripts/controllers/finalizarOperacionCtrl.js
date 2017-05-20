71d1fa331ef9efee3cb4f6b724d9dad1a31afe1d
Rocio <rbianchi34@gmail.com>
2017-05-20 15:40:53 -0300

    print

--- app/scripts/controllers/finalizarOperacionCtrl.js
+++ app/scripts/controllers/finalizarOperacionCtrl.js
@@ -2,9 +2,16 @@
 	'use strict';
 	angular
 	.module('cine')
-	.controller('finalizarOperacionCtrl', ['$scope','Datos', '$rootScope',
-	function($scope,Datos,$rootScope){		
-		$scope.funcion = Datos.listado();					
+	.controller('finalizarOperacionCtrl', ['$scope','Datos', '$rootScope', '$window',
+	function($scope,Datos,$rootScope,$window){		
+		$scope.funcion = Datos.listado();			
+		$scope.printDiv = function(divId) {
+		var printContents = document.getElementById(divId).innerHTML;
+		var popupWin = window.open('', '_blank', 'width=300,height=300');
+		popupWin.document.open()
+		popupWin.document.write('<html><head></head><body onload="window.print()">' + printContents + '</html>');
+		popupWin.document.close();
+		} 
 	}])
 })();
 
