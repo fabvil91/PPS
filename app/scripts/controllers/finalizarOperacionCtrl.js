b84b3cd6949093fe38c85ad4523e64d2e5fac642
Rocio <rbianchi34@gmail.com>
2017-05-21 17:15:47 -0300

    arregle imprimir

--- app/scripts/controllers/finalizarOperacionCtrl.js
+++ app/scripts/controllers/finalizarOperacionCtrl.js
@@ -5,13 +5,10 @@
 	.controller('finalizarOperacionCtrl', ['$scope','Datos', '$rootScope', '$window',
 	function($scope,Datos,$rootScope,$window){		
 		$scope.funcion = Datos.listado();			
-		$scope.printDiv = function(divId) {
-		var printContents = document.getElementById(divId).innerHTML;
-		var popupWin = window.open('', '_blank', 'width=300,height=300');
-		popupWin.document.open()
-		popupWin.document.write('<html><head></head><body onload="window.print()">' + printContents + '</html>');
-		popupWin.document.close();
-		} 
+		
+		$scope.imprimir=function(){
+			$window.print();
+		}
 	}])
 })();
 
