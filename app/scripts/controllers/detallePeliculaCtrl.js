e35fd3f3106004fb0058846818e8081121975f9f
Fabrizio Villella <fabrizio.villella.91@gmail.com>
2017-05-26 10:45:44 -0300

    Refactoring

--- app/scripts/controllers/detallePeliculaCtrl.js
+++ app/scripts/controllers/detallePeliculaCtrl.js
@@ -3,105 +3,9 @@
 	angular.module('cine')
 	.controller('detallePeliculaCtrl', ['$rootScope','$scope','Datos','$sce','Salas','Complejos','Formatos','Idiomas','Slides','Funciones',function($rootScope,$scope,Datos,$sce,Salas,
 		Complejos,Formatos,Idiomas,Slides,Funciones){	
-	/*	$scope.complejos = ["Cinemar Avellaneda", "Cinemar Lanus"];
-		$scope.formatos = ["2D","3D"];
-		$scope.idiomas = ["Español", "Subtitulado"];*/
-
+	
 		$scope.filtro = {};
-
-	/*	$scope.funciones = [
-			{
-				pelicula: {imageUrl: "images/peliculas/StarWars.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/sGbxmsDFVnE",
-			 			   nombre: "Star Wars",
-			 			   descripcion: "Episodio 7",
-			 			   genero: "Space Opera",
-						   duracion: "98",
-						   tituloOriginal: "Star Wars",
-						   director: "JJ Abrams",
-						   calificacion: "+13"},						 			   
-				formato: "2D",
-				complejo: {
-						   nombre: "Cinemar Avellaneda"
-						  },
-				idioma: "Español",
-				dia: new Date("5/23/2017"),
-				hora: new Date(2017,4,23,15,10,0,0)
-			},
-			{
-				pelicula: {imageUrl: "images/peliculas/Logan.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/Div0iP65aZo",
-			 			   nombre: "Logan",
-			 			   descripcion: "Vuelve de nuevo a la acción",
-			 			   genero: "Acción",
-						   duracion: "80",
-						   tituloOriginal: "Logan",
-						   director: "John Will",
-						   calificacion: "+16"},			 	
-				formato: "3D",
-				complejo: {
-						   nombre: "Cinemar Lanus"
-						  },
-				idioma: "Subtitulado",
-				dia: new Date("5/24/2017"),
-				hora: new Date(2017,4,24,16,30,0,0)
-			},
-			{
-				pelicula: {imageUrl: "images/peliculas/America.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/uVdV-lxRPFo",
-			 			   nombre: "Capitán América",
-			 			   descripcion: "El héroe de Estados Unidos",
-			 			   genero: "Acción",
-						   duracion: "76",
-						   tituloOriginal: "Captain America",
-						   director: "Mary Weild",
-						   calificacion: "+13"},			 	
-				formato: "3D",
-				complejo: {
-						   nombre: "Cinemar Avellaneda"
-						  },
-				idioma: "Español",
-				dia: new Date("5/25/2017"),
-				hora: new Date(2017,4,25,18,0,0,0)
-			},
-			{
-				pelicula: {imageUrl: "images/peliculas/BeautyAndTheBeast.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/SqQvZ_VUtg8",
-			 			   nombre: "Bella y Bestia",
-			 			   descripcion: "Un romance diferente",
-			 			   genero: "Romance",
-						   duracion: "71",
-						   tituloOriginal: "Beauty And The Beast",
-						   director: "Taliz Al Quilani",
-						   calificacion: "ATP"},							 			   
-				formato: "2D",
-				complejo: {
-						   nombre: "Cinemar Lanus"
-						  },
-				idioma: "Español",
-				dia: new Date("5/26/2017"),
-				hora: new Date(2017,4,26,10,0,0,0)
-			},
-			{
-				pelicula: {imageUrl: "images/peliculas/BeautyAndTheBeast.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/SqQvZ_VUtg8",
-			 			   nombre: "Bella y Bestia",
-			 			   descripcion: "Un romance diferente",
-			 			   genero: "Romance",
-						   duracion: "71",
-						   tituloOriginal: "Beauty And The Beast",
-						   director: "Taliz Al Quilani",
-						   calificacion: "ATP"},							 			   
-				formato: "2D",
-				complejo: {
-						   nombre: "Cinemar Lanus"
-						  },
-				idioma: "Español",
-				dia: new Date("5/26/2017"),
-				hora: new Date(2017,4,26,16,30,0,0)
-			}
-		];*/
-
+	
 		 Complejos.listado()
 	     .then(function(datos){
 	     	console.log(datos);
@@ -122,27 +26,29 @@
 				     	console.log(datos);
 
 				        $scope.funciones = datos;
-				
+						/* Filtramos solo las funciones desde la fecha actual */
 						(function(){						
 							var funcionesDesdeAhora = [];
 
 							for (var i = $scope.funciones.length - 1; i >= 0; i--) {				
 								$scope.funciones[i].diaTime = new Date($scope.funciones[i].dia).getTime();
+
 								if(new Date($scope.funciones[i].hora).getTime() >= new Date().getTime()){
 									funcionesDesdeAhora.push($scope.funciones[i]);
 								}
 							}
 							$scope.funciones = funcionesDesdeAhora;
-							console.log($scope.funciones);
-							console.log(funcionesDesdeAhora);
+							console.log($scope.funciones);							
 						})();
 						
+						/* Suma dias a una fecha */
 						Date.prototype.addDays = function(days) {
 				        var dat = new Date(this.valueOf())
 				        dat.setDate(dat.getDate() + days);
 				        return dat;
 				   		}
 
+				   		/* Devuelve las fechas entre dos limites */
 				   		function getDates(startDate, stopDate) {
 				      	var dateArray = new Array();
 				      	var currentDate = startDate;
@@ -153,6 +59,7 @@
 				      	return dateArray;
 				    	}
 
+				    	/* Generamos los elementos formateados para el combo de dias */
 						$scope.dias = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
 						var hoy = new Date();
 						hoy.setHours(0,0,0,0);
@@ -181,32 +88,30 @@
 							$scope.filtro = {};			
 						}
 						
+						/* Cargamos en los filtros lo seleccionado en la pantalla anterior (main) */
 						$scope.filtro.pelicula = Datos.listado().pelicula;
 						if(Datos.listado().filtroDia){			
 							$scope.filtro.diaNombre = $scope.dias[new Date(Datos.listado().dia).getDay()] + " - " + new Date(Datos.listado().dia).getDate() + "/" + (new Date(Datos.listado().dia).getMonth()+1);
 							$scope.filtro.diaLocale = new Date(Datos.listado().dia).getTime();
 						}
-						if(Datos.listado().filtroFormato){	
-							console.log(Datos.listado());								
-							$scope.filtro.formato = Datos.listado().formato._id;
-							console.log($scope.filtro.formato);
+						if(Datos.listado().filtroFormato){																
+							$scope.filtro.formato = Datos.listado().formato._id;							
 						}
 						if(Datos.listado().filtroIdioma){			
 							$scope.filtro.idioma = Datos.listado().idioma._id;
 						}
-						if(Datos.listado().filtroComplejo){
-							//$scope.filtro.complejo = {};			
+						if(Datos.listado().filtroComplejo){										
 							$scope.filtro.complejo = Datos.listado().complejo._id;
 						}
 
+						/* Generamos campo nuevo con dia formateado en la funcion */
 						(function(){
 							for (var i = 0; i < $scope.funciones.length; i++ ) {	   
 							  var funciones = $scope.funciones;
 				       		  funciones[i].diaFormateado = $scope.dias[new Date(funciones[i].dia).getDay()] + " - " + new Date(funciones[i].dia).getDate() + "/" + (new Date(funciones[i].dia).getMonth()+1);       		  
 							}
 							console.log($scope.funciones);	
-						})();
-					
+						})();					
 			  		})
 				    .catch(function(e){
 				      console.log(e);
@@ -215,12 +120,10 @@
 			     .catch(function(e){
 			       console.log(e);
 			     })
-
 		     })
 		     .catch(function(e){
 		       console.log(e);
 		     })
-
 	     })
 	     .catch(function(e){
 	       console.log(e);
@@ -228,8 +131,7 @@
 
 		$scope.tab = 1;
 
-        $scope.setTab = function (tabId) {
-        	console.log(Datos.listado());
+        $scope.setTab = function (tabId) {        	
             $scope.tab = tabId;
         };
 
@@ -237,13 +139,11 @@
             return $scope.tab === tabId;
         };
 
-        $scope.formatear = function(funcion){
-        	console.log(funcion);
+        $scope.formatear = function(funcion){        	
         	return funcion.replace(/,/g, " > ");
         }
 
-         $scope.formatearHora = function(funcion){
-        	console.log(funcion);
+         $scope.formatearHora = function(funcion){        	
         	var fecha = new Date(funcion.hora);
         	return fecha.getHours() + ":" + (fecha.getMinutes() == "0"? "00" : fecha.getMinutes());
         }
--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -2,219 +2,105 @@
 	'use strict';
 	angular.module('cine')
 	.controller('mainCtrl', ['$rootScope','$scope','Datos','Complejos','Formatos','Idiomas','Slides','Funciones',function($rootScope,$scope,Datos,Complejos,Formatos,Idiomas,Slides,Funciones){		
-	//	$scope.complejos = ["Cinemar Avellaneda", "Cinemar Lanus"];
+		 $scope.filtro = {};
+
+		 $scope.myInterval = 3000;
 
 		 Complejos.listado()
 	     .then(function(datos){
 	     	console.log(datos);
 	        $scope.complejos = datos;
+
+	        Formatos.listado()
+		    .then(function(datos){
+		    	console.log(datos);
+		        $scope.formatos = datos;
+
+		        Idiomas.listado()
+			    .then(function(datos){
+			     	console.log(datos);
+			        $scope.idiomas = datos;
+
+			        Slides.listado()
+				    .then(function(datos){
+				     	console.log(datos);
+				        $scope.slides = datos;
+
+				        Funciones.listado()
+					     .then(function(datos){
+					     	console.log(datos);
+
+					        $scope.funciones = datos;
+					        /* Filtramos solo las funciones desde la fecha actual */
+					        (function(){						
+							var funcionesDesdeAhora = [];
+
+							for (var i = $scope.funciones.length - 1; i >= 0; i--) {				
+								$scope.funciones[i].diaTime = new Date($scope.funciones[i].dia).getTime();
+
+								if(new Date($scope.funciones[i].hora).getTime() >= new Date().getTime()){
+									funcionesDesdeAhora.push($scope.funciones[i]);
+								}
+							}
+							$scope.funciones = funcionesDesdeAhora;
+							console.log($scope.funciones);			
+							})();
+							
+							/* Suma dias a una fecha */
+							Date.prototype.addDays = function(days) {
+					        var dat = new Date(this.valueOf())
+					        dat.setDate(dat.getDate() + days);
+					        return dat;
+					   		}
+
+					   		/* Devuelve las fechas entre dos limites */
+					   		function getDates(startDate, stopDate) {
+					      	var dateArray = new Array();
+					      	var currentDate = startDate;
+					      	while (currentDate <= stopDate) {
+					        	dateArray.push(currentDate)
+					        	currentDate = currentDate.addDays(1);
+					      	}
+					      	return dateArray;
+					    	}
+
+					    	/* Generamos los elementos formateados para el combo de dias */
+							$scope.dias = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
+							var hoy = new Date();
+							hoy.setHours(0,0,0,0);
+							var proxSem = (new Date()).addDays(6);
+							proxSem.setHours(0,0,0,0);
+									
+							$scope.fechas = getDates(hoy,proxSem);
+							$scope.fechasDias = [];
+
+							(function (){							
+								for (var i = 0; i < $scope.fechas.length; i++ ) {	   
+					       		  $scope.fechasDias.push($scope.dias[$scope.fechas[i].getDay()] + " - " + $scope.fechas[i].getDate() + "/" + ($scope.fechas[i].getMonth()+1));    
+								}						
+							})();  
+
+					     })
+					     .catch(function(e){
+					       console.log(e);
+					     });
+				    })
+				    .catch(function(e){
+				       console.log(e);
+				    })
+			    })
+			    .catch(function(e){
+			       console.log(e);
+			    })
+		    })
+		    .catch(function(e){
+		       console.log(e);
+		    })
 	     })
 	     .catch(function(e){
 	       console.log(e);
 	     })
-
-		//$scope.formatos = ["2D","3D"];
-
-		Formatos.listado()
-	     .then(function(datos){
-	     	console.log(datos);
-	        $scope.formatos = datos;
-	     })
-	     .catch(function(e){
-	       console.log(e);
-	     })
-
-		//$scope.idiomas = ["Español", "Subtitulado"];
-
-		Idiomas.listado()
-	     .then(function(datos){
-	     	console.log(datos);
-	        $scope.idiomas = datos;
-	     })
-	     .catch(function(e){
-	       console.log(e);
-	     })
-
-		$scope.filtro = {};
-
-		$scope.myInterval = 3000;
-		
-	/*	$scope.slides = [
-			{imageUrl: "images/carousel/starWars.jpg",
-			 nombre: "Star Wars",
-			 descripcion: "Episodio 7"
-			},
-			{imageUrl: "images/carousel/logan.jpg",
-			 nombre: "Logan",
-			 descripcion: "Vuelve de nuevo a la acción"
-			},
-			{imageUrl: "images/carousel/cv2.jpg",
-			 nombre: "Capitán América",
-			 descripcion: "El héroe de Estados Unidos"
-			},
-			{imageUrl: "images/carousel/beautyAndBeast.jpeg",
-			 nombre: "Bella y Bestia",
-			 descripcion: "Un romance diferente" 
-			}
-		];*/
-
-		Slides.listado()
-	     .then(function(datos){
-	     	console.log(datos);
-	        $scope.slides = datos;
-	     })
-	     .catch(function(e){
-	       console.log(e);
-	     })
-
-	/*	$scope.funciones = [
-			{
-				pelicula: {imageUrl: "images/peliculas/StarWars.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/sGbxmsDFVnE",
-			 			   nombre: "Star Wars",
-			 			   descripcion: "Episodio 7",
-			 			   genero: "Space Opera",
-						   duracion: "98",
-						   tituloOriginal: "Star Wars",
-						   director: "JJ Abrams",
-						   calificacion: "+13"},						 			   
-				formato: "2D",
-				complejo: {
-						   nombre: "Cinemar Avellaneda"
-						  },
-				idioma: "Español",
-				dia: new Date("5/23/2017"),
-				hora: new Date(2017,4,23,15,10,0,0)
-			},
-			{
-				pelicula: {imageUrl: "images/peliculas/Logan.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/Div0iP65aZo",
-			 			   nombre: "Logan",
-			 			   descripcion: "Vuelve de nuevo a la acción",
-			 			   genero: "Acción",
-						   duracion: "80",
-						   tituloOriginal: "Logan",
-						   director: "John Will",
-						   calificacion: "+16"},			 	
-				formato: "3D",
-				complejo: {
-						   nombre: "Cinemar Lanus"
-						  },
-				idioma: "Subtitulado",
-				dia: new Date("5/24/2017"),
-				hora: new Date(2017,4,24,16,30,0,0)
-			},
-			{
-				pelicula: {imageUrl: "images/peliculas/America.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/uVdV-lxRPFo",
-			 			   nombre: "Capitán América",
-			 			   descripcion: "El héroe de Estados Unidos",
-			 			   genero: "Acción",
-						   duracion: "76",
-						   tituloOriginal: "Captain America",
-						   director: "Mary Weild",
-						   calificacion: "+13"},			 	
-				formato: "3D",
-				complejo: {
-						   nombre: "Cinemar Avellaneda"
-						  },
-				idioma: "Español",
-				dia: new Date("5/25/2017"),
-				hora: new Date(2017,4,25,18,0,0,0)
-			},
-			{
-				pelicula: {imageUrl: "images/peliculas/BeautyAndTheBeast.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/SqQvZ_VUtg8",
-			 			   nombre: "Bella y Bestia",
-			 			   descripcion: "Un romance diferente",
-			 			   genero: "Romance",
-						   duracion: "71",
-						   tituloOriginal: "Beauty And The Beast",
-						   director: "Taliz Al Quilani",
-						   calificacion: "ATP"},							 			   
-				formato: "2D",
-				complejo: {
-						   nombre: "Cinemar Lanus"
-						  },
-				idioma: "Español",
-				dia: new Date("5/26/2017"),
-				hora: new Date(2017,4,26,10,0,0,0)
-			},
-			{
-				pelicula: {imageUrl: "images/peliculas/BeautyAndTheBeast.jpg",
-						   trailerUrl: "https://www.youtube.com/embed/SqQvZ_VUtg8",
-			 			   nombre: "Bella y Bestia",
-			 			   descripcion: "Un romance diferente",
-			 			   genero: "Romance",
-						   duracion: "71",
-						   tituloOriginal: "Beauty And The Beast",
-						   director: "Taliz Al Quilani",
-						   calificacion: "ATP"},							 			   
-				formato: "2D",
-				complejo: {
-						   nombre: "Cinemar Lanus"
-						  },
-				idioma: "Español",
-				dia: new Date("5/26/2017"),
-				hora: new Date(2017,4,26,16,30,0,0)
-			}
-		];*/
-
-		Funciones.listado()
-	     .then(function(datos){
-	     	console.log(datos);
-
-	        $scope.funciones = datos;
-
-	        (function(){						
-			var funcionesDesdeAhora = [];
-
-			for (var i = $scope.funciones.length - 1; i >= 0; i--) {				
-				$scope.funciones[i].diaTime = new Date($scope.funciones[i].dia).getTime();
-				if(new Date($scope.funciones[i].hora).getTime() >= new Date().getTime()){
-					funcionesDesdeAhora.push($scope.funciones[i]);
-				}
-			}
-			$scope.funciones = funcionesDesdeAhora;
-			console.log($scope.funciones);			
-			})();
-		
-			Date.prototype.addDays = function(days) {
-	        var dat = new Date(this.valueOf())
-	        dat.setDate(dat.getDate() + days);
-	        return dat;
-	   		}
-
-	   		function getDates(startDate, stopDate) {
-	      	var dateArray = new Array();
-	      	var currentDate = startDate;
-	      	while (currentDate <= stopDate) {
-	        	dateArray.push(currentDate)
-	        	currentDate = currentDate.addDays(1);
-	      	}
-	      	return dateArray;
-	    	}
-
-			$scope.dias = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
-			var hoy = new Date();
-			hoy.setHours(0,0,0,0);
-			var proxSem = (new Date()).addDays(6);
-			proxSem.setHours(0,0,0,0);
-					
-			$scope.fechas = getDates(hoy,proxSem);
-			$scope.fechasDias = [];
-
-			(function (){							
-				for (var i = 0; i < $scope.fechas.length; i++ ) {	   
-	       		  $scope.fechasDias.push($scope.dias[$scope.fechas[i].getDay()] + " - " + $scope.fechas[i].getDate() + "/" + ($scope.fechas[i].getMonth()+1));    
-				}						
-			})();  
-
-	     })
-	     .catch(function(e){
-	       console.log(e);
-	     });
-					
+	
 		$scope.filtrarDia = function(){									
 			var indice = $scope.fechasDias.indexOf($scope.filtro.dia);			
 			var dia = $scope.fechas[indice];
