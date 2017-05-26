6a4826846014f13323cb7e60237e83e5d93c3905
Fabrizio Villella <fabrizio.villella.91@gmail.com>
2017-05-26 18:12:49 -0300

    Comienzo de hacer Backend de Usuarios

---
+++ app/scripts/services/usuarios.js
@@ -0,0 +1,107 @@
+angular
+  .module('cine')
+  .service('Usuarios', ['$http', function($http){
+
+    this.listado = listado;
+    this.usuarioPorNombreUsuario = usuarioPorNombreUsuario;
+    this.articuloPrecioMayor = articuloPrecioMayor;
+    this.articuloPorProveedor = articuloPorProveedor;
+    this.articuloPorIndice = articuloPorIndice;
+    this.alta = alta;
+    this.modificar = modificar;
+    this.borrar = borrar;
+
+    function listado () {
+      return $http.get('http://localhost:3333/usuarios/getAll')
+      .then(function(rta){
+        return rta.data;
+      })
+      .catch(function(e){
+        return e;
+      })
+    }
+
+     function usuarioPorNombreUsuario(texto) {
+      return $http.get('http://localhost:3333/usuarios/username/'+texto)
+      .then(function(rta){
+        console.log(rta);
+        return rta.data;
+      })
+      .catch(function(e){
+        return e;
+      })
+    }
+
+    function articuloPrecioMayor(texto) {
+      return $http.get('http://localhost:3333/articulos/precio/'+texto)
+      .then(function(rta){
+        return rta.data;
+      })
+      .catch(function(e){
+        return e;
+      })
+    }
+
+    function articuloPorProveedor(texto) {
+      return $http.get('http://localhost:3333/articulos/proveedor/'+texto)
+      .then(function(rta){
+        return rta.data;
+      })
+      .catch(function(e){
+        return e;
+      })
+    }
+
+    function articuloPorIndice(texto) {
+      return $http.get('http://localhost:3333/articulos/indices/'+texto)
+      .then(function(rta){
+        return rta.data;
+      })
+      .catch(function(e){
+        return e;
+      })
+    }
+
+    function alta(item){
+      return $http({
+            url: 'http://localhost:3333/insertar',
+            method: "POST",
+            data: item,
+            headers: {'Content-Type': 'application/json'}})
+      .then(function(rta){
+        return rta.data;
+      })
+      .catch(function(e){
+        return e;
+      })
+    }
+
+    function modificar(item){
+      return $http({
+            url: 'http://localhost:3333/modificar',
+            method: "PUT",
+            data: item,
+            headers: {'Content-Type': 'application/json'}})
+      .then(function(rta){
+        return rta.data;
+      })
+      .catch(function(e){
+        return e;
+      })
+    }
+
+    function borrar(item){
+      return $http({
+            url: 'http://localhost:3333/eliminar',
+            method: "DELETE",
+            data: item,
+            headers: {'Content-Type': 'application/json'}})
+      .then(function(rta){
+        return rta.data;
+      })
+      .catch(function(e){
+        return e;
+      })
+    }
+
+  }]);
