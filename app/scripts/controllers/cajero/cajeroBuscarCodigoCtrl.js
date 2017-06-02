04e1070e8a0103374decee1127d47ee6b368620e
Fabrizio Villella <fabrizio.villella.91@gmail.com>
2017-06-02 17:31:14 -0300

    BuscarCodigo (Cajero). Casi terminado.

--- app/scripts/controllers/cajero/cajeroBuscarCodigoCtrl.js
+++ app/scripts/controllers/cajero/cajeroBuscarCodigoCtrl.js
@@ -7,14 +7,34 @@
         function ($scope,$location,Operaciones,Datos,$rootScope,Tarjetas,Bancos,Promociones) {
                 $scope.codigo = null;
                 $scope.operacion = null;                
-                $scope.funcion = Datos.listado();
+              
             
-                $scope.mes = [
-                              {nombre:"Enero", id:0},
-                              {nombre:"Febrero"},
-                              {nombre:"Marzo"},
-                              {nombre:"Abril"},
-                              {nombre:"Marzo"},
+                    Tarjetas.listado()
+                     .then(function(datos){
+                        console.log(datos);
+                        $scope.tarjetas = datos;
+
+                         Bancos.listado()
+                         .then(function(datos){
+                            console.log(datos);
+                            $scope.bancos = datos;
+
+                                 Promociones.listado()
+                                 .then(function(datos){
+                                    console.log(datos);
+                                    $scope.promociones = datos;
+
+                                        $scope.promociones = $scope.promociones.filter(function(element){
+                                            return (element.banco != null && element.tarjeta != null);
+                                        });
+                                        console.log($scope.promociones);
+
+                                             $scope.mes = [
+                                                {nombre:"Enero", id:0},
+                                                {nombre:"Febrero"},
+                                                {nombre:"Marzo"},
+                                                {nombre:"Abril"},
+                                                {nombre:"Marzo"},
                                                 {nombre:"Abril"},
                                                 {nombre:"Mayo"},
                                                 {nombre:"Junio"},
@@ -34,36 +54,65 @@
                                                 {nombre:"2022"},
                                                 {nombre:"2023"},
                                                 {nombre:"2024"}
-                                            ];
-
-                                          /*  $scope.funcion.operacion = {
-                                                vencimiento:{}
-                                            };*/
-
-                                          /*  $scope.hayPromo = false;
-                                            for (var i = $scope.funcion.entradas.length - 1; i >= 0; i--) {
-                                                if($scope.funcion.entradas[i].promocion && $scope.funcion.entradas[i].cantidad > 0){
-                                                    $scope.hayPromo = true;
-                                                    break;
-                                                }
-                                            }
-                                            console.log($scope.hayPromo);*/
+                                            ];                                                                                 
+                                  })
+                             .catch(function(e){
+                              console.log(e);
+                            })
+                        })
+                    .catch(function(e){
+                      console.log(e);
+                    })
+                 })
+                 .catch(function(e){
+                   console.log(e);
+                 })
+                              
+                $scope.cargar = function(){                
+                    console.log($scope.operacion);         
+                   // Datos.cargar(funcion); SI es efectivo, limpiar funcion.operacion (Solo se llena con pago en tarjeta)
+                }   
                                 
-            $scope.cargar = function(){                
-               // console.log($scope.tipoOperacion);        	
-               // Datos.cargar(funcion);
-        	   }	
+         
+               $scope.buscar = function(codigo){
 
-             $scope.buscar = function(codigo){
-              Operaciones.operacionPorCodigo(codigo)
-             .then(function(datos){
-              console.log(datos);
-                $scope.operacion = datos[0];
+                Operaciones.operacionPorCodigo(codigo)
+               .then(function(datos){
 
-                $scope.formatearHora = function(funcion){          
-                var fecha = new Date(funcion.hora);
-                 return fecha.getHours() + ":" + (fecha.getMinutes() == "0"? "00" : fecha.getMinutes());
-                }
+                  console.log(datos);
+                  $scope.operacion = datos[0];
+
+                     $scope.hayPromo = false;
+                     for (var i = $scope.operacion.funcion.entradas.length - 1; i >= 0; i--) {
+                      if($scope.operacion.funcion.entradas[i].promocion && $scope.operacion.funcion.entradas[i].cantidad > 0){
+                         $scope.hayPromo = true;
+                         break;
+                       }
+                      }
+                      console.log($scope.hayPromo);
+
+                      if($scope.operacion.funcion.transaccion.tipoTransaccion != 'compra'){
+                        $scope.operacion.funcion.operacion = {
+                            vencimiento:{}
+                         };
+                      }
+
+                      $scope.formatearHora = function(funcion){          
+                      var fecha = new Date(funcion.hora);
+                       return fecha.getHours() + ":" + (fecha.getMinutes() == "0"? "00" : fecha.getMinutes());
+                      }
+
+                      $scope.formatearEntrada = function(entrada){                          
+                       return entrada.tipo + " - " + entrada.monto + " - " + entrada.cantidad;
+                      }
+
+                      $scope.total = function(){  
+                      var total = 0;                        
+                       for (var i = $scope.operacion.funcion.entradas.length - 1; i >= 0; i--) {
+                         total = total + $scope.operacion.funcion.entradas[i].subtotal;
+                       }
+                       return total;
+                      }
 
                  })
                 .catch(function(e){
@@ -71,8 +120,6 @@
                 });            
            }
 
-            
-         
         }]) 
 })();
 
